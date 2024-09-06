4a6dea9f374c278059414b1eb9306b8cc17f0715
Grupo DB <dbrepositorio@outlook.com>
2024-09-06 18:03:51 -0300

    dia 06/09

--- src/app/pages/dashboardoperacoes/producao/britagem/homebritagem/homebritagem.component.ts
+++ src/app/pages/dashboardoperacoes/producao/britagem/homebritagem/homebritagem.component.ts
@@ -24,15 +24,19 @@ export class HomebritagemComponent implements OnInit {
   tnBritadaCalDia!:number;
   tnBritadaCalMes!:number;
   tnBritadaCalAno!:number;
-  teste!:any;
+  minaBritadorDia!:any;
+  minaBritadorMes!:any;
+  minaBritadorAno!:any;
   dados: any;
+  movimentos: any;
   constructor(
     private homeService: HomeService,
   ){}
 
   ngOnInit(): void {
     this.calcular();
-    this.paradas('atual');
+    this.paradas('mensal');
+    this.movimentacao();
     
   }
   calcular() {
@@ -64,8 +68,37 @@ export class HomebritagemComponent implements OnInit {
         { nome2: 'EMBUCHAMENTO(ROMPEDOR)', tempo2:response.embuchamento_rompedor_tempo, percent2:response.embuchamento_rompedor_percentual },
         { nome3: 'EMBUCHAMENTO(DESARME)', tempo3:response.embuchamento_desarme_tempo, percent3:response.embuchamento_desarme_percentual },
         { nome4: 'SETUP', tempo4:response.setup_tempo, percent4:response.setup_percentual },
+        { nome5: 'FALTA DE MATÉRIA PRIMA', tempo5:response.materiaprima_tempo, percent5:response.materiaprima_percentual },
+        { nome6: 'ESPERANDO DEMANDA', tempo6:response.esperando_demanda_tempo, percent6:response.esperando_demanda_percentual },
+        { nome7: 'PREPARADO LOCAL', tempo7:response.preparando_local_tempo, percent7:response.preparando_local_percentual },  
+        { nome8: 'EVENTO NÃO INFORMADO', tempo8: response.evento_nao_informado_tempo, percent8:response.evento_nao_informado_percentual },
+        { nome9: 'ALIMENTADOR DESLIGADO', tempo9: response.alimentador_desligado_tempo, percent9:response.alimentador_desligado_percentual },
       ]
     })
+  }
+
+  movimentacao(){
+    forkJoin({
+      atual: this.homeService.calcularBritagem('atual'),
+      mensal: this.homeService.calcularBritagem('mensal'),
+      anual: this.homeService.calcularBritagem('anual'),
+    }).subscribe( response =>{
+      // Respostas para cada tipo de cálculo
+    const respostaAtual = response.atual;
+    const respostaMensal = response.mensal;
+    const respostaAnual = response.anual;
+
+    this.minaBritadorDia = respostaAtual.mina_britador;
+    this.minaBritadorMes = respostaMensal.mina_britador;
+    this.minaBritadorAno = respostaAnual.mina_britador;  
+
+      this.movimentos = [
+        { origem1: 'MINA', destino1: 'BRITADOR', dia1:this.minaBritadorDia, mes1:this.minaBritadorMes, ano1:this.minaBritadorAno },
+      ]
+
+    })
+      
+
   }
 
 }
