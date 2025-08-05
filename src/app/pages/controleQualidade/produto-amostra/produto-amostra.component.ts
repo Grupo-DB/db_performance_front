866f7fe5b1e33001c3a3a33c3e319e340ff2e579
matheusricalde <matheusricaldee@gmail.com>
2025-08-05 09:56:33 -0300

    05-08-25 padronizaçao plano analise, analise, produto amostra

--- src/app/pages/controleQualidade/produto-amostra/produto-amostra.component.ts
+++ src/app/pages/controleQualidade/produto-amostra/produto-amostra.component.ts
@@ -27,7 +27,6 @@ import { ToastModule } from 'primeng/toast';
 import { LoginService } from '../../../services/avaliacoesServices/login/login.service';
 import { AmostraService } from '../../../services/controleQualidade/amostra.service';
 
-
 import { CardModule } from 'primeng/card';
 import { InplaceModule } from 'primeng/inplace';
 
@@ -49,54 +48,50 @@ export interface ProdutoAmostra {
 @Component({
   selector: 'app-produto-amostra',
   imports: [
-    ReactiveFormsModule,FormsModule,CommonModule,DividerModule,InputIconModule,
-    InputMaskModule,DialogModule,ConfirmDialogModule,SelectModule,IconFieldModule,
-    FloatLabelModule,TableModule,InputTextModule,InputGroupModule,InputGroupAddonModule,
-    ButtonModule,DropdownModule,ToastModule,NzMenuModule,DrawerModule,RouterLink,
-    InputNumberModule,AutoCompleteModule,MultiSelectModule, CardModule, InplaceModule
+    ReactiveFormsModule,FormsModule,CommonModule,DividerModule,InputIconModule,InputMaskModule,DialogModule,ConfirmDialogModule,SelectModule,IconFieldModule,FloatLabelModule,TableModule,InputTextModule,InputGroupModule,InputGroupAddonModule,ButtonModule,DropdownModule,ToastModule,NzMenuModule,DrawerModule,RouterLink,InputNumberModule,AutoCompleteModule,MultiSelectModule, CardModule, InplaceModule
   ],
   animations:[
   trigger('efeitoFade',[
-                                          transition(':enter',[
-                                            style({ opacity: 0 }),
-                                            animate('2s', style({ opacity:1 }))
-                                          ])
-                                        ]),
-                                        trigger('efeitoZoom', [
-                                          transition(':enter', [
-                                            style({ transform: 'scale(0)' }),
-                                            animate('2s', style({ transform: 'scale(1)' })),
-                                          ]),
-                                        ]),
-                                        trigger('bounceAnimation', [
-                                          transition(':enter', [
-                                            animate('4.5s ease-out', keyframes([
-                                              style({ transform: 'scale(0.5)', offset: 0 }),
-                                              style({ transform: 'scale(1.2)', offset: 0.5 }),
-                                              style({ transform: 'scale(1)', offset: 1 }),
-                                            ])),
-                                          ]),
-                                        ]),
-                                        trigger('swipeAnimation', [
-                                          transition(':enter', [
-                                            style({ transform: 'translateX(-100%)' }),
-                                            animate('1.5s ease-out', style({ transform: 'translateX(0)' })),
-                                          ]),
-                                          transition(':leave', [
-                                            style({ transform: 'translateX(0)' }),
-                                            animate('1.5s ease-out', style({ transform: 'translateX(100%)' })),
-                                          ]),
-                                        ]),
-                                        trigger('swipeAnimationReverse', [
-                                          transition(':enter', [
-                                            style({ transform: 'translateX(100%)' }),
-                                            animate('1.5s ease-out', style({ transform: 'translateX(0)' })),
-                                          ]),
-                                          transition(':leave', [
-                                            style({ transform: 'translateX(0)' }),
-                                            animate('1.5s ease-out', style({ transform: 'translateX(100%)' })),
-                                          ]),
-          ]),  
+    transition(':enter',[
+      style({ opacity: 0 }),
+      animate('2s', style({ opacity:1 }))
+    ])
+  ]),
+  trigger('efeitoZoom', [
+    transition(':enter', [
+      style({ transform: 'scale(0)' }),
+      animate('2s', style({ transform: 'scale(1)' })),
+    ]),
+  ]),
+  trigger('bounceAnimation', [
+    transition(':enter', [
+      animate('4.5s ease-out', keyframes([
+        style({ transform: 'scale(0.5)', offset: 0 }),
+        style({ transform: 'scale(1.2)', offset: 0.5 }),
+        style({ transform: 'scale(1)', offset: 1 }),
+      ])),
+    ]),
+  ]),
+  trigger('swipeAnimation', [
+    transition(':enter', [
+      style({ transform: 'translateX(-100%)' }),
+      animate('1.5s ease-out', style({ transform: 'translateX(0)' })),
+    ]),
+    transition(':leave', [
+      style({ transform: 'translateX(0)' }),
+      animate('1.5s ease-out', style({ transform: 'translateX(100%)' })),
+    ]),
+  ]),
+  trigger('swipeAnimationReverse', [
+    transition(':enter', [
+      style({ transform: 'translateX(100%)' }),
+      animate('1.5s ease-out', style({ transform: 'translateX(0)' })),
+    ]),
+    transition(':leave', [
+      style({ transform: 'translateX(0)' }),
+      animate('1.5s ease-out', style({ transform: 'translateX(100%)' })),
+    ]),
+  ]),  
   ],
   providers:[
     MessageService,ConfirmationService
