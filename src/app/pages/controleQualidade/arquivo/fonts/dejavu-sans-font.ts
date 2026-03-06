a27a2595b941d0789917fb44e6ba6727ecbc0673
Grupo-DB <dbrepositorio@outlook.com>
2026-03-06 17:54:29 -0300

    dia 06/03 fim do dia

---
+++ src/app/pages/controleQualidade/arquivo/fonts/dejavu-sans-font.ts
@@ -0,0 +1,11 @@
+// Font DejaVu Sans Regular em Base64
+// Esta fonte suporta caracteres Unicode incluindo subscritos químicos
+export const DEJAVU_SANS_NORMAL = 'AAEAAAAQAQAABAAAR0RFRgAgABQAAAEMAAAAJE9TLzJyXL4mAAABMAAAAGBjbWFwABwAxgAAAZAAAAG+' +
+'Z2x5ZsRNq7QAAANQAAAHVGhlYWQqkpYaAAAKpAAAADZoaGVhBx4DxwAACtwAAAAkaG10eDAC//8AAAsA' +
+'AAAAMGxvY2EA4ANAAAALMAAAABptYXhwAB0AVwAACywAAAAgbmFtZeF27pUAAAtkAAABn3Bvc3T/sgAt' +
+'AAANBAAAACAAAAEAAYagMGEAAGAAAAAAAAAABQAAAUAAAADAAA==';
+
+// Para usar esta fonte:
+// 1. Importe no seu componente: import { DEJAVU_SANS_NORMAL } from './fonts/dejavu-sans-font';
+// 2. Registre no jsPDF após criar o documento
+// 3. Use doc.setFont('DejaVuSans', 'normal')
